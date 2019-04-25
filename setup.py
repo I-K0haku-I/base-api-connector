606f184791b8ccf4421df8ad3883c070bb26152c
Andreas S <k0haku@hotmail.com>
2019-04-25 13:12:36 +0200

    add setup for compile and started module

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='base-api-connector',
+    version='0.0.0.1',
+    description='Generic Connector so you can create simple and readable classes for accessing APIs.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/I-K0haku-I/base-api-connector',
+    author='Andreas Schäfer',
+    author_email=None,
+    packages=find_packages(),
+    install_requires=['requests']
+)
