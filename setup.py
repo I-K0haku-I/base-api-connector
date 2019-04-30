771614722e66634cd22cacebd64a7ab3bdb26879
Andreas S <k0haku@hotmail.com>
2019-04-30 13:45:56 +0200

    version update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='base-api-connector',
-    version='0.0.0.1',
+    version='0.1.0',
     description='Generic Connector so you can create simple and readable classes for accessing APIs.',
     long_description=long_description,
     long_description_content_type='text/markdown',
